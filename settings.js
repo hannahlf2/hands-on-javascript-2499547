c9712f5e60cee173c0962acd08c10e31f34a0028
Hannah Freudenberger <hlfreudenberger@gmail.com>
2025-03-19 19:05:28 +0000

    section 3 chapter 2 changes

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 export default {
   // Get API key from https://home.openweathermap.org/users/sign_up
-  appid: [OpenWeatherAPIkey],
+  appid: "b8e773bf7e26f85db3ab2202cd0caf29",
   units: "metric",
-  location: "carpinteria,ca,us",
+  location: "denver,co,us",
 };
